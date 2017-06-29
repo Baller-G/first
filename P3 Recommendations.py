119a94f7fe7393d8d5393158b13e5610d701298c
ybalaji6 <yush.balaji@gmail.com>
2017-06-29 15:48:46 -0400

    g

--- P3 Recommendations.py
+++ P3 Recommendations.py
@@ -7,50 +7,65 @@ import numpy as np
 
 def findSimilar(iLike, userLikes):
     # Create an And similarity
-    similarityAnd = 0  # replace 0 with the correct code
+    similarityAnd = userLikes * iLike  # replace 0 with the correct code
     # Create a per user sum
-    similaritySum = 0  # replace 0 with the correct code
+    similarityAndSum = similarityAnd.sum(axis = 1)  # replace 0 with the correct code
     # Create an Or similarity
-    userSimilarityOr = 0  # replace 0 with the correct code
+    similarityOr = userLikes + iLike  # replace 0 with the correct code
     
     # Calculate the similarity
-    userSimilarity = 0  # replace 0 with the correct code to calculate the Jaccard Index for each user
+    userSimilarity = similarityAndSum / (similarityOr.su(axis = 1) - similarityAndSum)  # replace 0 with the correct code to calculate the Jaccard Index for each user
     
     # Make the most similar user has a new like that the previous user did not have
     # I used a while loop.
     # You can "get rid" of a user that is most similar, but doesn't have any new likes
     # by setting the userSimilarity for them to 0
     # When you get the index, save it in the variable maxIndex
-    
+    while True:
+        maxIndex = userSimilarity.argmax()
+        newLikes =  userLikes[maxIndex] - iLike
+        newCount = newLikes[newLikes > 0].sum()
+        if newCount > 0:
+            break
+        else:
+            userSimilartiy[maxIndex] = 0
     # Print the max similarity number (most times this is something like 0.17
-    
+    print("\nUser  Similarity: " + str(userSimilarity[maxIndex]))
     # Return the index of the user which is the best match
     return maxIndex
     
 def printMovie(id):
     # Print the id of the movie and the name.  This should look something like
     # "    - 430: Duck Soup (1933)" if the id is 430 and the name is Duck Soup (1933)
-    print(0)  # replace 0 with the correct code
+    print("    -" + str(id) + ": " + movieDict[id])  # replace 0 with the correct code
 
 def processLikes(iLike):
     print("\n\nSince you like:")
-    
+    for like in iLike:
+        printMovie(like)
     # Print the name of each movie the user reported liking
     # Hint: Use a for loop and the printMovie function.
 
     # Convert iLike into an array of 0's and 1's which matches the array for other users
     # It should have one column for each movie (just like the userLikes array)
     # Start with all zeros, then fill in a 1 for each movie the user likes
-    iLikeNp = 0  # replace 0 with the code to make the array of zeros
+    iLikeNp = np.zeros(maxMovie)
+    for i in iLike:
+        iLikeNp[i] = 1
+    # replace 0 with the code to make the array of zeros
     # You'll need a few more lines of code to fill in the 1's as needed
 
     # Find the most similar user
-    user = 0  # replace 0 with the correct code (hint: use one of your functions)
+    user = findSimilar(iLikeNp, userLikes)
+    # replace 0 with the correct code (hint: use one of your functions)
     print("\nYou might like: ")
     # Find the indexes of the values that are ones
     # https://stackoverflow.com/a/17568803/3854385 (Note: You don't want it to be a list, but you do want to flatten it.)
-    recLikes = 0  # replace 0 with the needed code
+    recLikes = np.argwhere(userLikes[user, :] == 1).flatten()  # replace 0 with the needed code
 
+    for like in reLikes:
+        if iLikeNp[like] == 0:
+            printMovie(like)
     # For each item the similar user likes that the person didn't already say they liked
     # print the movie name using printMovie (you'll also need a for loop and an if statement)
 
@@ -81,10 +96,11 @@ print(movieNames)
 
 # Create a dictionary to hold our temporary ratings
 movieRatingTemp = {}  # replace 0 with code for an empty dictionary
-for movie in movieData:
-    if movie['movie'] not in movieRatingTemp:
-        movieRatingTemp[movie['movie']] = []
-        movieRatingTemp[movie['movie']].append(movie['rating'])
+for row in movieData:
+    if row['movie'] not in movieRatingTemp:
+        movieRatingTemp[row['movie']] = [row['rating']]
+    else
+        movieRatingTemp[row['movie']].append(row['rating'])
 
 print(movieRatingTemp)
 
@@ -96,7 +112,7 @@ movieRating = {}  # replace 0 with code for an empty dictionary
 movieRatingCount = {}  # replace 0 with code for an empty dictionary
 
 for key in movieRatingTemp:
-    movieRating[key] = np.sum(movieRatingTemp[key])
+    movieRating[key] = np.mean(movieRatingTemp[key])
     movieRatingCount[key] = len(movieRatingTemp[key])
 
 # Using numpy place the average rating for each movie in movieRating and the total number of ratings in movieRatingCount
@@ -111,10 +127,18 @@ print("Top Ten Movies:")
 # Print the top 10 movies
 # It should print the number, title, id, rating and count of reviews for each movie
 # ie 2. Someone Else's America (1995) (ID: 1599) Rating: 5.0 Count: 1
-
+for i in range(0, 10):
+    print(
+        str(i + 1) + ". " + movieDict[movieRatingS[i][0]] + " (ID"
+    )
 
 # Top 10 Movies with at least 100 ratings    
 print("\n\nTop Ten movies with at least 100 ratings:")
+i = 0
+printCount = 0
+while printcount<10:
+    if movie Rating COunt[movieRatingS[i][0]] + "(ID: " + str(monieRatingS[i][0]) + ") Rating: " +
+        " Count: " + str 9movieRatingCOunt[movieRatingS[i][0]]
 # It should print the same thing, but this time all the movies should have over 100 ratings
 # The number should be the movie's absolute rank
 # ie (16. Close Shave, A (1995) (ID: 408) Rating: 4.49 Count: 112)
@@ -146,14 +170,16 @@ for id in iLike:
     iLikeNp[id] = 1
 
 # Find the max movie ID + 1
-maxMovie = np.max(movieNames['id']) + 1  # replace 0 with the correct code
+maxMovie = movieData['movie'].max() + 1  # replace 0 with the correct code
 
 # Find the max user Id + 1
 maxUser = np.max(movieData['user'])  # replace 0 with the correct code
 
 # Create an array of 0s which will fill in with 1s when a user likes a movie
-userLikes = np.zeros((maxUser, maxMovie))
-
+userLikes = np.zeros((movieData['user'].max() + 1, maxMovie))
+for row in movieData:
+    if row['rating'] == 4 or row['rating'] == 5:
+        userLikes[row['user'], row[movie]] = 1
 # Go through all the rows of the movie data.
 # If the user rated a movie as 4 or 5 set userLikes to 1 for that user and movie
 # Note: You'll need a for loop and an if statement
@@ -181,21 +207,42 @@ processLikes(iLike)
 iLike = [79,  96,  98, 168, 173, 176, 194, 318, 357, 427, 603, 1]
 processLikes(iLike)
 
+userMovies = 0
+
 while True:
+    run = raw_input("\nWould you like to get a recommendation (Y/N) ")
+    if len(run) == 0 or run[0] != 'Y':
+        break
+    iLike = []
+    whileTrue:
+    movie = raw_input("Enter a movie IDthat you like [1-" + str(maxMovie)  + "]" +
+                      " or leave a blank if done: ")
+
+'''while True:
     recommendation = raw_input("Would you like a movie recommendation? (y/n)")
     if recommendation == "y":
         while True:
-            if int(raw_input("What movie do you like?")) < 1683:
-                print("hi")
-                break
+            if userMovies > 0:
+                if raw_input("Do you like any other movies? (y/n)") == "y":
+                    userMovies = 0
+                elif raw_input("Do you like any other movies? (y/n)") == "n":
+                    print("hi")
+                    userMovies = 0
+                    break
+                else:
+                    print("That's not a valid answer.")
+            elif int(raw_input("What movie do you like?")) < 1683:
+                iLikeNp[int(raw_input("What movie do you like?"))] = 1
+                userMovies += 1
             else:
                 print("That is not a valid ID.")
     elif recommendation == "n":
-        print("Have a nice day.")
+        print('''
+              Have a nice day.
+              ''')
         break
     else:
-        print("That is an invalid answer.")
-
+        print("That is an invalid answer.")'''
 # If your code completes the above recommendations properly, you're ready for the last part,
 # allow the user to select any number of movies that they like and then give them recommendations.
 # Note: I recommend having them select movies by ID since the titles are really long.
